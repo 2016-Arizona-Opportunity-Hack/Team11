fff5aa5545afb34569a9ad980bb9f9eb3ed0746d
Peddi, Pranahitha <pranahitha.peddi@intel.com>
2016-10-01 21:26:13 -0700

    all models

---
+++ Inventory/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.Models
+{
+    public class Item
+    {
+        public int CategoryId { get; set; }
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Size { get; set; }
+        public string Age { get; set; }
+        public int LowLimit { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Inventory/Models/OrderDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public bool ItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
