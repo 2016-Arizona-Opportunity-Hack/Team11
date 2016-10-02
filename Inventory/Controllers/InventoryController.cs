c4571dcb7b0a0585093b9b2b185f93bee441e6a8
Ghanshyam Lele <ghanshyam.lele@gmail.com>
2016-10-02 04:11:42 -0700

    Get Items works

--- Inventory/Controllers/InventoryController.cs
+++ Inventory/Controllers/InventoryController.cs
@@ -1,30 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using Inventory.Models;
+
 
 namespace Inventory.Controllers
 {
-    public class InventoryController : Controller
+    public class InventoryController : BaseController
     {
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var strConnString = ConfigurationManager.ConnectionStrings["Development"].ConnectionString;
+            MySqlConnection mconn = new MySqlConnection(strConnString);
+            mconn.Open();
+            var results = GetItems(mconn);
+            /*
+                        InventoryViewModel inventoryVM = new InventoryViewModel()
+                        {
+                            Inventories = GetInventories(mconn),
+                            Items = GetItems(mconn),
+                            Locations = GetLocations(mconn),
+                            Categories = GetCategories(mconn)
+                        };
+            */
+            return View(results);
         }
     }
 }
