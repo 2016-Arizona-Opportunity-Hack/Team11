ed507f9d550699582426838e8fae912d7291ec70
Dhiren Navani <Dhiren Navani>
2016-10-02 00:21:36 -0700

    Create, Edit and Index

--- Inventory/Controllers/CategoryController.cs
+++ Inventory/Controllers/CategoryController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Inventory.Models;
 using MySql.Data.MySqlClient;
@@ -36,13 +34,75 @@ namespace Inventory.Controllers
             return View(result);
             //   return View();
         }
+
         public ActionResult Create()
         {
             var newcat = new Category();
-
             return View(newcat);
         }
 
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            CheckForDuplicate(category);
 
+            if (!ModelState.IsValid)
+                return View(category);
+            var strConnString = ConfigurationManager.ConnectionStrings["Development"].ConnectionString;
+
+            MySqlConnection mconn = new MySqlConnection(strConnString);
+            try
+            {
+                mconn.Open();
+
+                MySqlCommand command = mconn.CreateCommand();
+                command.CommandText = "Insert into Categories (CategoryName) values ('"+category.CategoryName+"')";
+                var output=command.ExecuteNonQuery();
+                if (output != 1) {
+                }
+            }
+            catch (Exception e) { }
+            finally {
+                if (mconn.State == System.Data.ConnectionState.Open)
+                    mconn.Close();
+            }
+
+                return RedirectToAction("Index");
+        }
+
+        private void CheckForDuplicate(Category category)
+        {
+            
+                if (category == null)
+                    return;
+
+                var strConnString = ConfigurationManager.ConnectionStrings["Development"].ConnectionString;
+
+                MySqlConnection mconn = new MySqlConnection(strConnString);
+            try
+            {
+                mconn.Open();
+
+                MySqlCommand command = mconn.CreateCommand();
+                command.CommandText = "select count(*) from Categories where categoryId = " + category.CategoryId;
+
+                //MySqlDataReader reader = command.ExecuteReader();
+
+                int rowscount = Convert.ToInt32(command.ExecuteScalar());
+                
+
+                if (rowscount > 0)
+                    ModelState.AddModelError("Category Name", "A Category with this name already exists.");
+            }
+            catch (Exception e)
+            {
+                
+            }
+            finally {
+                if(mconn.State == System.Data.ConnectionState.Open)
+                    mconn.Close();   
+            }
+        }
     }
+
 }
