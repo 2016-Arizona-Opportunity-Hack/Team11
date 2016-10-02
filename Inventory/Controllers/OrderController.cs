60428c9e3fa7ac30f2dd6236c6a4c38f3581a8fc
ggunnam <gitanjali.gunnam@intel.com>
2016-10-01 17:42:00 -0700

    Sample Order Controller

--- Inventory/Controllers/OrderController.cs
+++ Inventory/Controllers/OrderController.cs
@@ -13,5 +13,31 @@ namespace Inventory.Controllers
         {
             return View();
         }
+
+        
+        public ActionResult Create()
+        {
+            var category = new Category
+            {
+                IsEnabled = true
+            };
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Name,IsEnabled,SequenceNumber,Description")] Category category)
+        {
+            //if (!ModelState.IsValid)
+            //    return View(category);
+
+            //category.Name = category.Name.NullTrim();
+            //category.Description = category.Description.NullTrim();
+
+            //_db.Categories.Add(category);
+            //_db.SaveChanges();
+
+            //return RedirectToAction("Index");
+        }
     }
 }
